4d6577ca1b241ded043dcacc404b992c638e4bf2
RichA <rich_ay@sbcglobal.net>
2020-10-22 23:44:38 -0500

    Finished category-routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,3 +1,5 @@
+
+// Dependencies
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
@@ -34,21 +36,64 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
+  .then( dbData => res.json(dbData) )
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////
 router.post('/', (req, res) => {
-  // create a new category
+  // Create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // Update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  // Delete a category by its `id` value
+  Category.destroy( {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData => {
+    if (!dbData) {
+      res.status(404).json({ message: 'No Category found with this id' });
+      return;
+    }
+    res.json(dbData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 
