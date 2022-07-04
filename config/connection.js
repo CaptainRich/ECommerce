b4a5855b20fd3ceee14635086ecad0f159d31fdd
RichA <rich_ay@sbcglobal.net>
2022-07-04 14:44:25 -0500

    Updaing comments for clearer code.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
+
+// Load the environment variables setup with the DOTENV package
 require('dotenv').config();
 
+// Import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+// If the on-line URL exists, use it.  Otherwise, pull the data locally from the "env" file.
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -12,4 +16,6 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Export the connection to the database.
 module.exports = sequelize;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// Collect all of the API routes into a single package.
 const categoryRoutes = require('./category-routes');
 const productRoutes  = require('./product-routes');
 const tagRoutes      = require('./tag-routes');
@@ -8,4 +9,5 @@ router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+/////////////////////////////////////////////////////////////////////////
 module.exports = router;
