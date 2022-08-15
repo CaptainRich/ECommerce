c366b02b3bc959820e5ab99dc1ace75f8da17b0e
RichA <rich_ay@sbcglobal.net>
2022-08-14 21:50:10 -0500

    Refactoring

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -96,8 +96,7 @@ router.post('/', (req, res) => {
       // If no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productTagIds) => 
-      res.status(200).json(productTagIds))
+    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
